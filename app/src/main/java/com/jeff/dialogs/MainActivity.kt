91c5c2e1560bb053fca8fe798f88f0b38961cc86
yaojie4433520 <yaojie4433520@qq.com>
2018-11-17 13:36:10 +0800

    添加注释

--- app/src/main/java/com/jeff/dialogs/MainActivity.kt
+++ app/src/main/java/com/jeff/dialogs/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.jeff.dialogs
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.support.v7.app.AppCompatActivity
+/**
+ * author : Jeff  5899859876@qq.com
+ * Created :  2018-11-17.
+ * description ：
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
